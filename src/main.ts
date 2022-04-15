fcff9a8934d7ff359fbca61f08070ca41ab2a63b
Drew Ruppel <druppel@goldencomm.com>
2022-04-14 21:00:17 -0700

    development

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Config from './Core/Config'
 import ChannelService from './Core/Services/ChannelService'
-import {Client, Intents, Interaction, TextChannel} from 'discord.js'
+import {Client, Intents, Interaction} from 'discord.js'
 import {SlashCommandBuilder} from '@discordjs/builders'
 import {REST} from '@discordjs/rest'
 import {Routes} from 'discord-api-types/v10'
@@ -25,7 +25,10 @@ if (config.GUILD_ID == null) {
 
 // Start Client
 const client = new Client(
-  { intents: [Intents.FLAGS.GUILDS] }
+  {
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+  }
 )
 
 // Channel Service @todo: move
@@ -34,8 +37,8 @@ const channelService = new ChannelService(client)
 // @todo: Get Commands
 const commands = [
   new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-  new SlashCommandBuilder().setName('donate-ore').setDescription('Ore Donation'),
-  new SlashCommandBuilder().setName('donate-pi').setDescription('PI Donation')
+  new SlashCommandBuilder().setName('donate').setDescription('Material(s) donation for the corporation'),
+  new SlashCommandBuilder().setName('ship-order').setDescription('Order your ship from the corporation'),
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(config.TOKEN)
@@ -58,24 +61,15 @@ client.on('ready', () => {
     '| __|__ __ ___       | __| __ | |_   ___  ___  ___\n' +
     '| _| \\ V // -_)      | _| / _||   \\ / _ \\/ -_)(_-/\n' +
     '|___| \\_/ \\___|      |___|\\__||_||_|\\___/\\___|/__/\n'
+  console.log(hello)
 
-  channelService.inspect('bot-testing')
-
-  const boot = client.channels.cache.find(channel => channel.id === '874351397443895388')
-
-  if (boot && boot.isText()) {
-    boot.send(hello).then(r => r).catch(e => console.error(e))
-  }
-
-/*  channelService.find('874351397443895388')
-    .then(r => console.log(r))
-    .catch(e => console.error(e))*/
+  client.user.setActivity('Assimilating')
 
   void client.application.commands.set(commands);
 })
 
-client.on('message', msg => {
-  console.log(`[message log] ${msg.content}`)
+client.on('messageCreate', message => {
+  console.log(`[message log] ${message.content}`)
 })
 
 client.on('interactionCreate', async (interaction: Interaction) => {
@@ -83,8 +77,16 @@ client.on('interactionCreate', async (interaction: Interaction) => {
 
   const { commandName } = interaction;
 
+  console.log(interaction)
+
   if (commandName === 'ping') {
-    await interaction.reply('Pong!');
+    await interaction.reply('Pong!')
+  }
+  if (commandName === 'donate') {
+    await interaction.reply('Donate your ore!')
+  }
+  if (commandName === 'ship-order') {
+    await interaction.reply('Ship Order stuff!')
   }
 });
 
