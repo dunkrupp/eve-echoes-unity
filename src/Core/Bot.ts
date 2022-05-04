e338e4fd641b7dcdc41304bf8aa735d420e8c743
Drew Ruppel <druppel@goldencomm.com>
2022-05-03 21:57:14 -0700

    update bot

--- src/Core/Bot.ts
+++ src/Core/Bot.ts
@@ -1,9 +1,15 @@
+import {ColorResolvable} from "discord.js";
+
 class Bot {
-  constructor (
-    name: string,
-    color: string,
-    thumbnail: string
-  ) {}
+  public image: string;
+  public color: ColorResolvable;
+  public name: string;
+
+  constructor () {
+    this.name = 'Unity'
+    this.color = '#a5000e' // Neutral 82a1a1
+    this.image = 'https://cdn.discordapp.com/avatars/953713248430071848/a9d231c6cee081db1206255596a5dffa.png'
+  }
 }
 
 export default Bot
